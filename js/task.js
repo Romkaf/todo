6515262a2b71795373529ee36bc83e0a3dac66ff
Roman <romkaft@mail.ru>
2020-07-01 23:00:15 +0400

    Completed js

--- js/task.js
+++ js/task.js
@@ -2,7 +2,7 @@
 
 const mainInput = document.querySelector('.todo__input');
 const todoList = document.querySelector('.todo__list');
-const todoItems = todoList.children;
+// const todoItems = todoList.querySelectorAll('.todo__item');
 const todoCounter = document.querySelector('.todo__counter b');
 const filterAll = document.querySelector('.filter-all');
 const filterActive = document.querySelector('.filter-active');
@@ -10,19 +10,41 @@ const filterCompleted = document.querySelector('.filter-completed');
 const footer = document.querySelector('.todo__footer');
 const btnClearCompleted = footer.querySelector('.todo__clear-completed');
 
-
 // Создаём задания после ввода в mainInput
-const createTodoItem = () => {
+
+const createTodoItem = (itemInfo) => {
   const Template = document.querySelector('template');
   const todoElement = Template.content.querySelector('.todo__item').cloneNode(true);
-  todoElement.querySelector('.todo__item-task').value = mainInput.value;
-  mainInput.value = "";
-  todoList.appendChild(todoElement);
-  // console.dir(todoElement);
+  todoElement.querySelector('.todo__item-task').value = itemInfo.value;
+  todoElement.querySelector('.todo__item-choice').checked =  itemInfo.checked;
+  const newClass = itemInfo.classOfItem ? itemInfo.classOfItem : "";
+  todoElement.classList += ` ${newClass}`;
+  todoElement.id = itemInfo.id;
+  return todoElement;
 }
 
-let counter;
+const renderTodoItems = (array) => {
+  todoList.innerHTML = "";
+  const fragment = document.createDocumentFragment();
+  // let array;
+  if (filterAll.checked) {
+    array = allItems;
+  } else if (filterActive.checked) {
+    array = activeItems;
+  } else if (filterCompleted.checked) {
+    array = completedItems;
+  }
+
+  for (let i = 0; i < array.length; i++) {
+    fragment.appendChild(createTodoItem(array[i]));
+  }
+  todoList.appendChild(fragment);
+};
+
 let allItems = [];
+let activeItems;
+let completedItems;
+let counter;
 const keyEnter = 13;
 
 const checkCounter = () => {
@@ -33,100 +55,124 @@ const checkCounter = () => {
   }
 }
 
-checkCounter();
-
-const count = (number) => {
-  counter = counter + number;
+const count = () => {
+  checkCounter();
+  allItems.forEach((item) => {
+    if (!item.checked) {
+      counter += 1;
+    }
+  });
   todoCounter.textContent = counter;
 };
 
-// const createCompletedItems = () => {
-//   let todoItem = todoList.querySelectorAll('.todo__item');
-//   for (let elem of todoItem) {
-//     if (elem.classList.contains('todo__item--completed')) {
-//       completedItems.push(elem);
-//     } else {
-//       activeItems.push(elem);
-//     }
-//   }
-// }
-
-
 const createAllItems = () => {
-  // let todoItem = todoList.querySelectorAll('.todo__item');
-  for (let elem of todoItems) {
-    if (!(allItems.includes(elem, 0))) {
-      allItems.push(elem);
-      // console.log(allItems);
-    }
-  }
+  let itemInfo = {};
+  itemInfo.checked = false;
+  itemInfo.value = mainInput.value;
+  itemInfo.id = allItems.length ? allItems[allItems.length-1].id + 1 : 1;
+  itemInfo.classOfItem ="";
+  allItems.push(itemInfo);
+  mainInput.value = "";
 }
 
-// const createArraysOfItems = () => {
-//   let todoItem = todoList.querySelectorAll('.todo__item');
-//   for (let elem of todoItem) {
-//     if (!(allItems.includes(elem, 0))) {
-//             allItems.push(elem);
-//     }
-//     if (elem.classList.contains('todo__item--completed')) {
-//       if (!(completedItems.includes(elem, 0))) {
-//         completedItems.push(elem);
-//       }
-//     } else {
-//       if (!(activeItems.includes(elem, 0))) {
-//         activeItems.push(elem);
-//       }
-//     }
-//   }
-// }
+const createActiveItems = () => {
+  return activeItems = allItems.filter((item) => !item.checked);
+};
 
-const showFooter = () => {
+const createCompletedItems = () => {
+  return completedItems = allItems.filter((item) => item.checked);
+};
+
+const visibilityFooter = () => {
   if (footer.classList.contains('hidden')) {
     footer.classList.remove('hidden');
   }
-}
-
-const hideFooter = () => {
-  if (todoItems.length === 0) {
+  if (allItems.length === 0) {
     footer.classList.add('hidden');
   }
 }
 
 const mainInputHandler = (evt) => {
   if ((evt.keyCode === keyEnter || evt.type === 'blur') && mainInput.value) {
-    createTodoItem();
-    count(1);
-    showFooter();
     createAllItems();
+    createActiveItems();
+    createCompletedItems();
+    renderTodoItems();
+    count();
+    checkChoiseAllItems();
+    visibilityChoiseAllItems();
+    visibilityFooter();
     onLocalStorage();
   }
 };
 
 mainInput.addEventListener('keydown', mainInputHandler);
-
 mainInput.addEventListener('blur', mainInputHandler);
+
+// Переключаем задания из активных в завершенные
+
+const selectItem = (evt) => {
+  let item = evt.target.parentNode;
+  const selectedTodoIndex = allItems.findIndex(element => element.id == item.id);
+  if (item.classList.contains('todo__item--completed')) {
+    item.classList.remove('todo__item--completed');
+    allItems[selectedTodoIndex].checked = false;
+    allItems[selectedTodoIndex].classOfItem = "";
+  } else {
+    item.classList.add('todo__item--completed');
+    allItems[selectedTodoIndex].checked = true;
+    allItems[selectedTodoIndex].classOfItem = "todo__item--completed";
+  }
+  renderActiveAndCompleted();
+}
+
+const renderActiveAndCompleted = () => {
+  createActiveItems();
+  createCompletedItems();
+  if (filterActive.checked || filterCompleted.checked) {
+    renderTodoItems();
+  }
+};
+
+const visibilityBtnClearCompleted = () => {
+  btnClearCompleted.style.visibility = (completedItems.length === 0) ? "hidden" : "visible";
+}
+
+todoList.addEventListener('click', (evt) => {
+  let target = evt.target; 
+  if (target.classList.contains("todo__item-choice")) { 
+    selectItem(evt);
+    count();
+    checkChoiseAllItems();
+    visibilityBtnClearCompleted();
+    onLocalStorage();
+  }
+});
 
 // удаляем задания поcле нажатия на крестик
 
 const deleteTodoItem = (evt) => {
   let target = evt.target; 
   if (target.classList.contains("todo__item-delete")) { 
-    target.parentNode.remove();
-    if (!target.parentNode.classList.contains('todo__item--completed')) {
-      count(-1);
-    }
+    const deletedTodoIndex = allItems.findIndex(item => item.id == target.parentNode.id);
+    allItems.splice(deletedTodoIndex, 1);
   }
 }
 
 todoList.addEventListener('click', (evt) => {
-  deleteTodoItem(evt);
-  // onLocalStorage();
-  hideFooter();
+  if (evt.target.classList.contains("todo__item-delete")) {
+    deleteTodoItem(evt);
+    createActiveItems();
+    createCompletedItems();
+    renderTodoItems();
+    count();
+    onLocalStorage();
+    visibilityChoiseAllItems();
+    visibilityFooter();
+  }
 });
 
 // Редактирование при двойном клике
-
-let itemInputs = todoList.getElementsByClassName('.todo__item-task');
 
 const onEditingItemInputs = function(evt) {
   const target = evt.target;
@@ -142,8 +188,14 @@ const onEditingItemInputs = function(evt) {
 
 const offEditingItemInputs = (evt) => {
   if (evt.keyCode === keyEnter || evt.type === 'blur') {
+    allItems.forEach((item) => {
+      if (item.id == evt.target.parentNode.id) {
+        item.value = evt.target.value;
+      }
+    });
     evt.target.parentNode.classList.remove('todo__item--editing');
     evt.target.setAttribute('disabled', 'disabled');
+    onLocalStorage();
   }
 }
 
@@ -153,161 +205,116 @@ todoList.addEventListener('dblclick', function(evt) {
   evt.target.addEventListener('keydown', offEditingItemInputs);
 });
 
-// Переключаем задания из активных в завершенные
-
-const choiceItem = (evt) => {
-  let item = evt.target.parentNode;
-  if (item.classList.contains('todo__item--completed')) {
-    item.classList.remove('todo__item--completed');
-    count(1);
-    if (filterCompleted.checked) {
-      item.classList.add('hidden');
-    }
-    
-  } else {
-    item.classList.add('todo__item--completed');
-    count(-1);
-    if (filterActive.checked) {
-      item.classList.add('hidden');
-    }
-  }
-}
-
-const changeVisibilityBtnClearCompleted = () => {
-  btnClearCompleted.style.visibility = "visible";
-  const checkCompletedItems = ()=> {
-    let items = Array.from(todoItems);
-    return items.every((item) => {
-      return !(item.classList.contains("todo__item--completed"));  
-    });
-  }
-  if (checkCompletedItems()) {
-    btnClearCompleted.style.visibility = "hidden";
-  }
-}
-
-todoList.addEventListener('click', (evt) => {
-  let target = evt.target; 
-  if (target.classList.contains("todo__item-choice")) { 
-    choiceItem(evt);
-    changeVisibilityBtnClearCompleted();
-    // onLocalStorage();
-  }
-});
-
 // Показываем все задания при нажатии на "Все", активные задания при нажатии на "Активные" и завершенные при нажатии на "Завершенные"
 
 const showAll = () => {
-  for (let elem of todoItems) {
-      elem.classList.remove('hidden');
-  }
+  renderTodoItems();
 }
 
 filterAll.addEventListener('click', showAll);
   
-const showActive = () => {
-  for (let elem of todoItems) {
-    if (elem.classList.contains('todo__item--completed')) {
-      elem.classList.add('hidden');
-    } else {
-      elem.classList.remove('hidden');
-    }
-  }  
+const showActive = () => { 
+  renderTodoItems(createActiveItems());
 }
 
 filterActive.addEventListener('click', showActive);
 
-const showCompleted = () => {
-  for (let elem of todoItems) {
-    if (elem.classList.contains('todo__item--completed')) {
-      elem.classList.remove('hidden');
-    } else {
-      elem.classList.add('hidden');
-    }
-  }  
+const showCompleted = () => { 
+  renderTodoItems(createCompletedItems());
 }
 
 filterCompleted.addEventListener('click', showCompleted);
 
 // Удаляем завершенные после нажатия на "удалить завершенные"
 
-
-
 const clearCompletedItems = () => {
-  let items = todoList.querySelectorAll('.todo__item')
-  for (let elem of items) {
-    if (elem.classList.contains('todo__item--completed')) {
-      elem.remove(elem);
+  for (let i = allItems.length - 1; i >=0 ; i--) {
+    if (allItems[i].checked) {
+          allItems.splice(i, 1);
     }
   }
+  createCompletedItems();
   btnClearCompleted.style.visibility = "hidden";
 }
 
 btnClearCompleted.addEventListener('click' , () => {
   clearCompletedItems();
-  hideFooter();
+  renderTodoItems();
+  visibilityChoiseAllItems();
+  visibilityFooter();
+  onLocalStorage();
+});
+
+// Отмечаем все задания при нажатии на .choice-all
+
+const choiseAllItems = document.querySelector('.todo__choice-all');
+const labelOfChoiseAllItems = document.querySelector('.todo__choice-all + label');
+
+const selectAllItems = () => {
+  if (allItems.every((item) => item.checked == true)) {
+    allItems.forEach((item) => {
+      item.checked = false;
+      item.classOfItem = "";
+    });
+    for (let elem of todoList.children) {
+      elem.classList = ' todo__item';
+      elem.children[0].checked = false;
+    }
+  } else {
+    allItems.forEach((item) => {
+      item.checked = true;
+      item.classOfItem = "todo__item--completed";
+    });
+    for (let elem of todoList.children) {
+      elem.classList += ' todo__item--completed';
+      elem.children[0].checked = true;
+    }
+  }
+  renderActiveAndCompleted();
+};
+
+const visibilityChoiseAllItems = () => {
+  labelOfChoiseAllItems.style.visibility = (allItems.length === 0) ? "hidden" : "visible";
+};
+
+const checkChoiseAllItems = () => {
+  labelOfChoiseAllItems.style.opacity = allItems.every((item) =>  item.checked === true) ? 0.8 : 0.2;
+};
+
+choiseAllItems.addEventListener('click', () => {
+  selectAllItems();
+  visibilityBtnClearCompleted();
+  count();
+  checkChoiseAllItems();
+  onLocalStorage();
 });
 
 // LocalStorage
 
 document.querySelector('.todo__heading').addEventListener('click', () => {
-  const counter = footer.querySelector('.todo__counter b');
-  localStorage.removeItem('todos', todoList.innerHTML);
-  localStorage.removeItem('values', todoItems);
-  localStorage.removeItem('counter', counter.innerHTML);
+  localStorage.removeItem('todos');
 })
 
 const onLocalStorage = () => {
-  // const counter = footer.querySelector('.todo__counter b');
-  // localStorage.setItem('todos', todoList.innerHTML);
-  // localStorage.setItem('todos', todoItems);
-  // const tasks = todoList.querySelectorAll('.todo__item-task');
-  // localStorage.setItem('values', tasks);
-  let hp = JSON.stringify(allItems);
-  console.log(hp);
-  localStorage.setItem('todos', hp);
-  // localStorage.setItem('counter', counter.innerHTML);
-  // console.log(localStorage.todos);
+  const tasks = JSON.stringify(allItems);
+  localStorage.setItem('todos', tasks);
 }
 
-const remember = () => {
-  // const itemsLocStor = localStorage.getItem('todos');
-  console.log(itemsLocStor);
-  for (let i = 0; i < itemsLocStor.length-1; ++i) {
-    createTodoItem();
-    console.log(itemsLocStor);
-    // todoItems[i].children[2].value = itemsLocStor[i].children[2].value;
-    // todoItems[i].children[0].checked = itemsLocStor[i].children[0].checked;
-  }  
-}
-
-function loadTodos() {
-  const itemsLocStor = JSON.parse(localStorage.getItem('todos'));
-  // const counterLocStor = localStorage.getItem('counter');
-  if (itemsLocStor) {
-    // footer.querySelector('.todo__counter b').innerHTML = counterLocStor;
-    // showFooter();
-    // console.dir(itemsLocStor); 
+const loadTodos = () => {
+  const tasks = JSON.parse(localStorage.getItem('todos'));
+  if (tasks) {
+    allItems = tasks;
+    renderTodoItems(tasks);
+    console.log(allItems);
+    count();
+    visibilityChoiseAllItems();
+    checkChoiseAllItems();
+    visibilityFooter();
+    createCompletedItems();
+    visibilityBtnClearCompleted();
   }
-  console.dir(itemsLocStor); 
-  // const deleteButtons = document.querySelectorAll("span.todo-trash");
-  // for (const button of deleteButtons) {
-  //     listenDeleteTodo(button);
-  // }
 }
 
 loadTodos();
 
-//  function ft() {
-//    let data = [];
-
-//   for (let elem of todoItems) {
-//     let value = elem.children[2].value;
-//     let checkbox = elem.children[0].checked;
-//   }
-//  };   
-let array = [1, 2, 3];
-
-localStorage.setItem('array', JSON.stringify(array));
-console
 
